e17f25f7cdc612d36174db40eb3547fad9efedba
fbehrens51 <fbehrens@vmware.com>
2021-02-18 17:56:07 -0500

    initial commit

---
+++ runStack.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -ex
+curr_dir=$PWD
+
+export AWS_DEFAULT_REGION="us-west-1"
+#1.5  issue is that it doesn't support resolvers, so it will fail when ELB changes underlying IPs until you restart it
+#USER_DATA=$(cat haproxy_cloud_init.yml)
+#USER_DATA=$(cat haproxy2_cloud_init.yml)
+USER_DATA=$(cat haproxy18_cloud_init.yml)
+
+jq --arg v "$USER_DATA"  '(.[] | select(.ParameterKey=="UserData") | .ParameterValue) |= $v' cf_params.json >tmp.json
+
+aws cloudformation describe-stacks --stack-name haproxy-tcp >> /dev/null || EXIT_CODE=$?
+
+if [[ $EXIT_CODE -eq 0 ]]
+then
+  aws cloudformation update-stack --stack-name haproxy-tcp --template-body file://${curr_dir}/haproxy_cf.json --parameters file://${curr_dir}/tmp.json
+else
+  aws cloudformation create-stack --stack-name haproxy-tcp --template-body file://${curr_dir}/haproxy_cf.json --parameters file://${curr_dir}/tmp.json --on-failure DELETE
+fi
